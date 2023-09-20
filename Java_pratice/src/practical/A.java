dc1801989b3f0a69e1b77b38ca860587b78292fb
RahulJaikr <Rahul_Jain_@outlook.com>
2023-09-20 15:01:52 +0530

    My second trail

---
+++ Java_pratice/src/practical/A.java
@@ -0,0 +1,33 @@
+package practical;
+
+public class A 
+{
+   void show() 
+   {
+	System.out.println("Im learnig Oop's concept");   
+   }
+   
+}  
+   class B extends A
+{  
+
+      public static void main(String[] args) 
+    {
+    	//System.out.println("We are intelligent students ");
+    	B d = new B();
+    	d.show();
+    }
+}
+    
+    
+
+	   
+   
+	
+	
+	
+	
+	
+	
+
+
